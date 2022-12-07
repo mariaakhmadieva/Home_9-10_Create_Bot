e6044d7530ac28aab0f91a58ff8a6f7ffd7145aa
Maks <nordmax69@gmail.com>
2022-12-07 10:38:07 +0700

    added processing

--- our_bot.py
+++ our_bot.py
@@ -10,7 +10,6 @@ import os
 load_dotenv()
 secret_token = os.getenv('TOKEN')
 bot = TeleBot(secret_token)
-
 value = ''
 old_value = ''
 
@@ -53,6 +52,38 @@ def get_message(message):
         bot.send_message(message.from_user.id, '0', reply_markup=keyboard)
     else:
         bot.send_message(message.from_user.id, value, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def main(query):
+    global value, old_value 
+    data = query.data
+
+    if data == 'no':
+        pass
+    elif data == 'c':
+        value = ''
+    elif data == '<=':
+        if value != '':
+            value = value[:len(value)-1]
+    elif data == '=':
+        try:
+            value = str( eval(value) )
+        except:
+            value = 'error'
+    else:
+        value += data
+
+    if (value != old_value and value != '') or ('0' != old_value and value == ''):
+        if value == '':
+            bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.message_id, text='0', reply_markup=keyboard)
+            old_value = '0'
+        else:
+            bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.message_id, text=value, reply_markup=keyboard)
+            old_value = value
+    
+    
+    if value == 'error': value = ''
+
 
 
 bot.polling()
